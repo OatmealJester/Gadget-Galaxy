ced83fe96f80de05d5474d604ed0f009e71fcce6
mridulpahwa <mpahw002@fiu.edu>
2023-10-23 20:16:17 -0400

    Product info

---
+++ src/utils/MenuList - copy.js
@@ -0,0 +1,85 @@
+import Headphones from '../assets/ProductImages/Headphones_1.png';
+import BoseHeadphones from '../assets/ProductImages/Headphones_2.png';
+import HPspectre from '../assets/ProductImages/Laptop_1.png';
+import ROGzephyrus from '../assets/ProductImages/Laptop_2.png';
+import SamsungFold from '../assets/ProductImages/Phone_1.png';
+import Iphone15 from '../assets/ProductImages/Phone_2.png';
+
+export const MenuList = [
+  {
+    name: 'ASUS ROG Laptop',
+    price: '1400',
+    detail: 'Ram ',
+    image: ROGzephyrus,
+  },
+  {
+    name: 'iPhone 15',
+    price: '1000',
+    detail: 'New Camera',
+    image: Iphone15,
+  },
+  {
+    name: 'Samsung Galaxy',
+    price: '900',
+    detail: 'Nice Phone',
+    image: SamsungFold,
+  },
+  {
+    name: 'Headphones',
+    price: '50',
+    detail: 'Sounds Nice',
+    image: Headphones,
+  },
+  {
+    name: 'HP Spectre',
+    price: '985',
+    detail: 'RAM',
+    image: HPspectre,
+  },
+  {
+    name: 'Bose Headphone',
+    price: '245',
+    detail: 'Heavy Basse',
+    image: BoseHeadphones,
+  },
+  {name:"BLU-G53 PEARL",
+   price:'110',
+   detail:'Dual MegaPixel Camera',
+   image: BluG53,
+
+  },
+  {
+   name:'Boost Moto G',
+   price:'200',
+   detail:'Ultra Wide Max Vision Display',
+   image:BoostMotoG,
+
+  }
+  {
+   name:'Galaxy A03',
+   price:'100',
+   detail:' Octa-core 1.6GHz processing power',
+   image:GalaxyA03,
+
+  }
+  {
+    name:'Moterolla G-Plus',
+    price:'150',
+    detials:'Dual AutoFocus Pixel',
+    image:MoterollaGPlus,
+
+  }
+  {
+    name:'OneNote',
+    price:'170',
+    details:'Gaming Tools',
+    image:OneNote,
+  }
+  {
+    name:'Pixel 7 Pro',
+    price:'1100',
+    details:'telephoto capability'
+    image:Pixel7Pro,
+  }
+
+];
